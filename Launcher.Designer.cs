417b30bebffc18e450c92bd9b9cd40399614ec1d
Kausar Mehmood <khan.kausar@gmail.com>
2019-10-20 23:51:32 +0500

    Initial commit

---
+++ Launcher.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace GameLauncher
+{
+    partial class Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.buttonMin = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.linkLabelNews = new System.Windows.Forms.LinkLabel();
+            this.linkHome = new System.Windows.Forms.LinkLabel();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.progressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelContainer.BackgroundImage = global::GameLauncher.Properties.Resources.background;
+            this.panelContainer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelContainer.Controls.Add(this.buttonMin);
+            this.panelContainer.Controls.Add(this.buttonClose);
+            this.panelContainer.Controls.Add(this.linkLabelNews);
+            this.panelContainer.Controls.Add(this.linkHome);
+            this.panelContainer.Controls.Add(this.buttonPlay);
+            this.panelContainer.Controls.Add(this.progressBarDownload);
+            this.panelContainer.Controls.Add(this.panel1);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(1214, 719);
+            this.panelContainer.TabIndex = 0;
+            // 
+            // buttonMin
+            // 
+            this.buttonMin.Location = new System.Drawing.Point(1139, 13);
+            this.buttonMin.Name = "buttonMin";
+            this.buttonMin.Size = new System.Drawing.Size(27, 23);
+            this.buttonMin.TabIndex = 6;
+            this.buttonMin.Text = "-";
+            this.buttonMin.UseVisualStyleBackColor = true;
+            this.buttonMin.Click += new System.EventHandler(this.buttonMin_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(1175, 13);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(27, 23);
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // linkLabelNews
+            // 
+            this.linkLabelNews.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkLabelNews.AutoSize = true;
+            this.linkLabelNews.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabelNews.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelNews.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabelNews.LinkColor = System.Drawing.Color.White;
+            this.linkLabelNews.Location = new System.Drawing.Point(214, 73);
+            this.linkLabelNews.Name = "linkLabelNews";
+            this.linkLabelNews.Size = new System.Drawing.Size(85, 29);
+            this.linkLabelNews.TabIndex = 4;
+            this.linkLabelNews.TabStop = true;
+            this.linkLabelNews.Text = "NEWS";
+            this.linkLabelNews.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelNews_LinkClicked);
+            // 
+            // linkHome
+            // 
+            this.linkHome.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkHome.AutoSize = true;
+            this.linkHome.BackColor = System.Drawing.Color.Transparent;
+            this.linkHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkHome.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkHome.LinkColor = System.Drawing.Color.White;
+            this.linkHome.Location = new System.Drawing.Point(76, 73);
+            this.linkHome.Name = "linkHome";
+            this.linkHome.Size = new System.Drawing.Size(85, 29);
+            this.linkHome.TabIndex = 3;
+            this.linkHome.TabStop = true;
+            this.linkHome.Text = "HOME";
+            this.linkHome.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkHome_LinkClicked);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.BackColor = System.Drawing.Color.Transparent;
+            this.buttonPlay.BackgroundImage = global::GameLauncher.Properties.Resources.button_on;
+            this.buttonPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonPlay.FlatAppearance.BorderSize = 0;
+            this.buttonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlay.Location = new System.Drawing.Point(983, 620);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(196, 68);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.UseVisualStyleBackColor = false;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // progressBarDownload
+            // 
+            this.progressBarDownload.Location = new System.Drawing.Point(49, 620);
+            this.progressBarDownload.Name = "progressBarDownload";
+            this.progressBarDownload.Size = new System.Drawing.Size(912, 23);
+            this.progressBarDownload.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::GameLauncher.Properties.Resources.Logo;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(49, 264);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(378, 321);
+            this.panel1.TabIndex = 0;
+            // 
+            // Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1214, 719);
+            this.Controls.Add(this.panelContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Launcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher";
+            this.panelContainer.ResumeLayout(false);
+            this.panelContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.ProgressBar progressBarDownload;
+        private System.Windows.Forms.LinkLabel linkHome;
+        private System.Windows.Forms.LinkLabel linkLabelNews;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonMin;
+    }
+}
+
---
+++ Launcher.cs
@@ -0,0 +1,151 @@
+﻿using GameLauncher.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameLauncher
+{
+    public partial class Launcher : Form
+    {
+
+        const string NewsURL = "https://eu.middlearth.net";
+        const string HomeURL = "https://eu.middlearth.net/forum/forum";
+        const string UpdatesURL = "https://eu.middlearth.net/middleearth/versions/";
+        readonly string DownloadsPath = System.IO.Path.GetTempPath();
+        const string GameExeFile = "Tibia.exe";
+        private string GamePath
+        {
+            get
+            {
+                string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).ToLower();
+                path = path.Replace("\\bin\\debug", "");
+                return path;
+            }
+        }
+
+        private int CurrentVersion
+        {
+            get
+            {
+                string version = "";
+                int versionId = 0;
+
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\MiddleEarth");
+                if (key != null)
+                {
+                    version = key.GetValue("Version").ToString();
+                }
+
+                if (!string.IsNullOrEmpty(version))
+                {
+                    versionId = Convert.ToInt32(version);
+                }
+                return versionId;
+            }
+            set
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\MiddleEarth", true))
+                {
+                    key.SetValue("Version", value);
+                }
+            }
+        }
+
+
+
+        WebClient client = null;
+
+        public Launcher()
+        {
+            InitializeComponent();
+            this.panelContainer.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+
+       
+        private string CheckForUpdates()
+        {
+
+            string latestFileName = "";
+            bool isUpdateAvailable = true;
+
+            if(isUpdateAvailable)
+            {
+                buttonPlay.BackgroundImage = Resources.button_off;
+            }
+            else
+            {
+                buttonPlay.BackgroundImage = Resources.button_on;
+            }
+            return latestFileName;
+        }
+
+        private void StartDownload(string fileName)
+        {
+            string updatesURL = UpdatesURL + fileName;
+            string downloadPath = DownloadsPath;
+
+            if (!downloadPath.EndsWith("\\"))
+            {
+                downloadPath += "\\";
+            }
+            downloadPath += fileName;
+
+            client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            client.DownloadFileAsync(new Uri(updatesURL), downloadPath);
+        }
+
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progressBarDownload.Maximum = (int)e.TotalBytesToReceive / 100;
+            this.progressBarDownload.Value = (int)e.BytesReceived / 100;
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+
+        }
+
+        private void linkHome_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo(HomeURL);
+            Process.Start(sInfo);
+        }
+
+        private void linkLabelNews_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo(NewsURL);
+            Process.Start(sInfo);
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            string fileName = CheckForUpdates();
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                StartDownload(fileName);
+            }
+        }
+    }
+}
